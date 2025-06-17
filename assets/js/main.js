0e4805318ecb60650db44eb8a2fcf17ccbbb32f3
jobaerlisedu <hsjobaer.lis.edu@outlook.com>
2025-06-17 11:08:38 +0600

    style changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -199,3 +199,12 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+
+  /**
+   * Frequently Asked Questions Toggle
+   */
+  document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle').forEach((faqItem) => {
+    faqItem.addEventListener('click', () => {
+      faqItem.parentNode.classList.toggle('faq-active');
+    });
+  });
